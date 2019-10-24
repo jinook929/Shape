c47c990b3e8d03c81124ef0e53232a5afa9b97f4
jinook929 <jinook929@gmail.com>
2019-10-23 20:58:35 -0400

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int x, int y) {
+        this(x, y, 0, 0);
+    }
+}
